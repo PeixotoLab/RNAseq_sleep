131aa87fbd38eb7dc2189530a83b6a3f5fc44ecb
Stephanie Hicks <stephaniechicks@gmail.com>
2021-02-11 19:26:24 -0500

    updating code to build bulk salmon index for Katie

--- 01_quantification/build-index-salmon.sh
+++ 01_quantification/build-index-salmon.sh
@@ -8,14 +8,14 @@
 ########## Files here are for bulk ############
 # to build salmon index
 # Use
-# #$ -l mem_free=5G,h_vmem=5G
+# #$ -l mem_free=10G,h_vmem=10G
 ###############################################
 
 # create salmon index with decoys (this process takes ~1 hour for mouse)
-# salmon index -t /fastscratch/myscratch/shicks1/RNAseq_sleep/salmon_index_files/gentrome_transcripts_mouse.fa.gz \
-#              -d /fastscratch/myscratch/shicks1/RNAseq_sleep/salmon_index_files/decoys_mouse.txt \
-#              -i /fastscratch/myscratch/shicks1/RNAseq_sleep/salmon_index_files/gencode.vM25-salmon-index-v1.0.0-mouse-withdecoys \
-#              --gencode --threads 4 -k 31
+salmon index -t /fastscratch/myscratch/shicks1/RNAseq_sleep/salmon_index_files/gentrome_transcripts_mouse.fa.gz \
+             -d /fastscratch/myscratch/shicks1/RNAseq_sleep/salmon_index_files/decoys_mouse.txt \
+             -i /fastscratch/myscratch/shicks1/RNAseq_sleep/salmon_index_files/gencode.vM25-salmon-index-v1.0.0-mouse-withdecoys \
+             --gencode --threads 4 -k 31
 
 # create salmon index without decoys (this process takes <30 mins for mouse)
 # salmon index -t /fastscratch/myscratch/shicks1/RNAseq_sleep/salmon_index_files/gencode.vM25.transcripts.fa.gz \
@@ -31,10 +31,10 @@
 # #$ -l mem_free=10G,h_vmem=10G
 ###############################################
 
-cd /fastscratch/myscratch/shicks1/RNAseq_sleep/salmon_index_files
+# cd /fastscratch/myscratch/shicks1/RNAseq_sleep/salmon_index_files
 
-# create salmon index with decoys (this process takes ~1 hour for mouse)
-salmon index -t <(cat gencode.vM25.annotation.expanded.fa GRCm38.primary_assembly.genome.fa) \
-             -d GRCm38.primary_assembly.genome.chrnames.txt \
-             -i gencode.vM25-salmon-index-v1.0.0-mouse-withdecoys-annotation-expanded \
-             --gencode --threads 4 -k 31
+# # create salmon index with decoys (this process takes ~1 hour for mouse)
+# salmon index -t <(cat gencode.vM25.annotation.expanded.fa GRCm38.primary_assembly.genome.fa) \
+#              -d GRCm38.primary_assembly.genome.chrnames.txt \
+#              -i gencode.vM25-salmon-index-v1.0.0-mouse-withdecoys-annotation-expanded \
+#              --gencode --threads 4 -k 31
--- 01_quantification/create-decoys-salmon.sh
+++ 01_quantification/create-decoys-salmon.sh
@@ -16,7 +16,7 @@ grep "^>" <(gunzip -c GRCm38.primary_assembly.genome.fa.gz) | cut -d " " -f 1 > 
 sed -i.bak -e 's/>//g' decoys_mouse.txt
 
 # 2. Along with the list of decoys salmon also needs the concatenated transcriptome and genome reference file for index. NOTE: the genome targets (decoys) should come after the transcriptome targets in the reference
-cat gencode.vM25.transcripts.mouse.fa.gz GRCm38.primary_assembly.genome.fa.gz > gentrome_transcripts_mouse.fa.gz
+cat gencode.vM25.transcripts.fa.gz GRCm38.primary_assembly.genome.fa.gz > gentrome_transcripts_mouse.fa.gz
 
 
 ###############################################
