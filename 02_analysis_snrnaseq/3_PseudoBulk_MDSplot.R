10e0ea03419b273666daa8aa73a138ea33d9a8fe
zuinelena3 <68145836+zuinelena3@users.noreply.github.com>
2023-09-28 15:07:27 +0200

    Update PseudoBulk_MDSplot.R

--- 02_analysis_snrnaseq/3_PseudoBulk_MDSplot.R
+++ 02_analysis_snrnaseq/3_PseudoBulk_MDSplot.R
@@ -1,100 +1,123 @@
+# Pseudo - bulk ####
+# The goal in this analysis is to create pseudo-bulk
+
+# Set up
 library(SingleCellExperiment)
 library(scuttle)
 library(muscat)
 library(edgeR)
 library(ggplot2)
+library(patchwork)
 
-# Pseudo-bulk ####
-sce.obj <- readRDS("snRNA_SCE_CellAnnotation.rds")
-# We remove the cell-type with less than 500 cells
-sce.obj <- sce.obj[,!c(sce.obj$Azimuth.labels=="Car3"|sce.obj$Azimuth.labels=="Endo"|sce.obj$Azimuth.labels=="Lamp5"|
-                   sce.obj$Azimuth.labels=="Sncg"|sce.obj$Azimuth.labels=="Sst Chodl"|sce.obj$Azimuth.labels=="VLMC")]
+# Load SCE object
+sce_obj <- readRDS("snrna_sce_annot.rds")
+# The cell-types with more than 500 cells were selected
+sce_obj <- sce_obj[, !c(sce_obj$azimuth_labels == "Car3" |
+                          sce_obj$azimuth_labels == "Endo" |
+                          sce_obj$azimuth_labels == "Lamp5" |
+                          sce_obj$azimuth_labels == "Sncg" |
+                          sce_obj$azimuth_labels == "Sst Chodl" |
+                          sce_obj$azimuth_labels == "VLMC")]
 
-pseudo.bulk <- aggregateAcrossCells(sce.obj, use.assay.type = "counts", id=DataFrame(label=sce.obj$Azimuth.labels, sample=sce.obj$sample_id))
-colnames(pseudo.bulk) <- paste(pseudo.bulk$Azimuth.labels, pseudo.bulk$sample_id, sep="_")
-pseudo.bulk <- logNormCounts(pseudo.bulk)
+# Note: if scuttle::logNormCounts doesn't work, remake SingleCellExpression object.
 
-# We create class variable
-pseudo.bulk$class <- pseudo.bulk$Azimuth.labels
-pseudo.bulk$class[which(grepl("CTX", pseudo.bulk$class))] <- "Glutamatergic"
+# Pseudo-bulk creation
+snrna_pb <- aggregateAcrossCells(sce_obj, use.assay.type = "counts", 
+                                 id = DataFrame(label = sce_obj$azimuth_labels, 
+                                                sample = sce_obj$sample_id))
+colnames(snrna_pb) <- paste(snrna_pb$azimuth_labels, snrna_pb$sample_id, sep = "_")
+snrna_pb <- logNormCounts(snrna_pb)
 
-pseudo.bulk$class[which(grepl("-", pseudo.bulk$class))] <- "Other"
-pseudo.bulk$class[!(pseudo.bulk$class=="Astro"|pseudo.bulk$class=="Oligo"|pseudo.bulk$class=="Glutamatergic"|pseudo.bulk$class=="Other")] <- "GABAergic"
+# Cell-type class variable was created
+snrna_pb$class <- snrna_pb$azimuth_labels
+snrna_pb$class[which(grepl("CTX", snrna_pb$class))] <- "Glutamatergic"
 
-# Save pseudo-bulk
-saveRDS(pseudo.bulk, file = "snRNA_PseudoBulk.rds")
+snrna_pb$class[which(grepl("-", snrna_pb$class))] <- "Other"
+
+snrna_pb$class[!(snrna_pb$class == "Astro" | snrna_pb$class == "Oligo" |
+                   snrna_pb$class == "Glutamatergic" |
+                   snrna_pb$class == "Other")] <- "GABAergic"
+
+# Pseudo-bulk object was saved
+saveRDS(snrna_pb, file = "snrna_pb.rds")
 
 # MDS plot ####
-# If this error appears: Error in element_line(linewidth = 0.2, color = "lightgrey") :unused argument (linewidth = 0.2)
-source("pbMDSfunction.R")
+# Load Allen color palette
+load("/mnt/callisto/Zuin/AllenColorLabel.RData")
 
-load("AllenColorLabel.RData")
-# by neuronal subclass
-prep.SCE <- prepSCE(pseudo.bulk[,c(pseudo.bulk$class=="Glutamatergic"|pseudo.bulk$class=="GABAergic")],
-                    kid = "Azimuth.labels", # subpopulation assignments
-                    gid = "condition",  # group IDs 
-                    sid = "sample_id",   # sample_id IDs 
+# Load the code inside pbMDSfunction.R file if this error appears:
+# "Error in element_line(linewidth = 0.2, color = "lightgrey") :unused argument (linewidth = 0.2)"
+source("/mnt/callisto/Zuin/pbMDSfunction.R")
+
+neuronal_color <- subclass_color[-c(1:3, 12:14, 16, 17, 19, 21)]
+
+# MDS plot according to the Glutamatergic labels
+prep_sce <- prepSCE(snrna_pb[, snrna_pb$class == "Glutamatergic"],
+                    kid = "azimuth_labels", # subpopulation assignments
+                    gid = "condition",  # group IDs
+                    sid = "sample_id",   # sample_id IDs
                     drop = TRUE)
+pb <-  aggregateData(prep_sce, assay = "logcounts",
+                     by = c("cluster_id", "sample_id"))
 
-pb <-  aggregateData(prep.SCE, assay="logcounts",by = c("cluster_id", "sample_id"))
+mds_gluta <- pbMDS(pb) + scale_color_manual(values = neuronal_color) +
+  labs(col = "Cell-types", shape = "Condition") +
+  theme(axis.text = element_text(size = 7), axis.title = element_text(size = 7),
+        legend.position = "none")
 
-neuronal.color <- subclass.color[-c(1:3,12:14,16,17,19,21)] # remove Non-Neuronal labels
+# MDS plot according to the GABA-ergic labels
+prep_sce <- prepSCE(snrna_pb[, snrna_pb$class == "GABAergic"],
+                    kid = "azimuth_labels", gid = "condition",
+                    sid = "sample_id", drop = TRUE)
+pb <-  aggregateData(prep_sce, assay = "logcounts",
+                     by = c("cluster_id", "sample_id"))
 
-mds_plot <- pbMDS(pb) + scale_color_manual(values=neuronal.color)
-ggsave(mds_plot, file=paste("snRNA_MDSplot_neuronal_subclass.pdf",sep = ""), width = 20, height = 20, units = "cm")
+mds_gaba <- pbMDS(pb) + scale_color_manual(values = neuronal_color) +
+  labs(col = "Cell-types", shape = "Condition") +
+  theme(axis.text = element_text(size = 7), axis.title = element_text(size = 7),
+        legend.position = "none")
 
-# by GABA-ergic labels 
-prep.SCE <- prepSCE(pseudo.bulk[,pseudo.bulk$class=="GABAergic"],
-                    kid = "Azimuth.labels", # subpopulation assignments
-                    gid = "condition",  # group IDs 
-                    sid = "sample_id",   # sample_id IDs 
-                    drop = TRUE)
-pb <-  aggregateData(prep.SCE, assay="logcounts",by = c("cluster_id", "sample_id"))
+patched <- (mds_gluta | mds_gaba)
+ggsave("Peixoto_Figure4_part2.jpg",  width = 18, height = 9, units = "cm")
+ggsave("Peixoto_Figure4_part2.pdf",  width = 18, height = 9, units = "cm")
 
-gaba.color <- neuronal.color[c(9:11)] 
+# MDS plot according to the Non-Neuronal labels
+prep_sce <- prepSCE(snrna_pb[, (snrna_pb$class == "Astro" |
+                                  snrna_pb$class == "Oligo" |
+                                  snrna_pb$class == "Other")],
+                    kid = "azimuth_labels", gid = "condition",
+                    sid = "sample_id", drop = TRUE)
+pb <-  aggregateData(prep_sce, assay = "logcounts", 
+                     by = c("cluster_id", "sample_id"))
 
-mds_plot <- pbMDS(pb) + scale_color_manual(values=gaba.color)
-ggsave(mds_plot, file=paste("snRNA_MDSplot_GABAergic.pdf",sep = ""), width = 20, height = 20, units = "cm")
+mds_other <- pbMDS(pb) + scale_color_manual(values = subclass_color) +
+  labs(col = "Cell-types", shape = "Condition") +
+  theme(legend.spacing.y = unit(0, "cm"), axis.text = element_text(size = 7),
+        axis.title = element_text(size = 7), legend.title = element_text(size = 7),
+        legend.text = element_text(size = 7), legend.key.size = unit(0.4, "cm"))
 
-# by Glutamatergic labels 
-prep.SCE <- prepSCE(pseudo.bulk[,pseudo.bulk$class=="Glutamatergic"],
-                    kid = "Azimuth.labels", # subpopulation assignments
-                    gid = "condition",  # group IDs 
-                    sid = "sample_id",   # sample_id IDs 
-                    drop = TRUE)
-pb <-  aggregateData(prep.SCE, assay="logcounts",by = c("cluster_id", "sample_id"))
+# MDS plot according to the neuronal labels on negative control genes
+# Load negative control genes
+negctrl <- read.table("SD_Negative_Controls.txt")
+# Negative control gene was selected inside the pseudo-bulk object
+pb_negctrl <- snrna_pb[rownames(snrna_pb) %in% negctrl$x, ]
 
-gluta.color <- neuronal.color[-c(9:11)] 
+prep_sce <- prepSCE(pb_negctrl[, !(pb_negctrl$class == "Astro" |
+                                     pb_negctrl$class == "Oligo" |
+                                     pb_negctrl$class == "Other")],
+                    kid = "azimuth_labels", gid = "condition",
+                    sid = "sample", drop = TRUE)
+pb <-  aggregateData(prep_sce, assay = "logcounts", 
+                     by = c("cluster_id", "sample_id"))
 
-mds_plot <- pbMDS(pb) + scale_color_manual(values=gluta.color)
-ggsave(mds_plot, file=paste("snRNA_MDSplot_Glutamatergic.pdf",sep = ""), width = 20, height = 20, units = "cm")
+neuronal_color <- subclass_color[c(2:9, 12, 14:15)]
 
-# by Non-Neuronal subclass
-prep.SCE <- prepSCE(pseudo.bulk[,(pseudo.bulk$class=="Astro"|pseudo.bulk$class=="Oligo"|pseudo.bulk$class=="Other")],
-                    kid = "Azimuth.labels", # subpopulation assignments
-                    gid = "condition",  # group IDs 
-                    sid = "sample_id",   # sample_id IDs 
-                    drop = TRUE)
-pb <-  aggregateData(prep.SCE, assay="logcounts",by = c("cluster_id", "sample_id"))
+mds_negctrl <- pbMDS(pb) + scale_color_manual(values = subclass_color) +
+  labs(col = "Cell-types", shape = "Condition") +
+  theme(legend.spacing.y = unit(0, "cm"), axis.text = element_text(size = 7),
+        axis.title = element_text(size = 7), legend.title = element_text(size = 7),
+        legend.text = element_text(size = 7), legend.key.size = unit(0.4, "cm"))
 
-non.neuronal.color <- subclass.color[-c(2:12,15,17:21)] # select Non-Neuronal labels
-
-mds_plot <- pbMDS(pb) + scale_color_manual(values=non.neuronal.color)
-ggsave(mds_plot, file=paste("snRNA_MDSplot_NonNeuronal_subclass.pdf",sep = ""), width = 20, height = 20, units = "cm")
-
-# MDS plot on Negative Controls
-NegCtrls <- read.table("SD_Negative_Controls.txt") #Negative Controls from Microarray
-
-# We select negative controls 
-pseudo.neg <- pseudo.bulk[rownames(pseudo.bulk) %in% NegCtrls$x,]
-
-# by neuronal subclass
-prep.SCE <- prepSCE(pseudo.neg[,!(pseudo.neg$class=="Astro"|pseudo.neg$class=="Oligo"|pseudo.neg$class=="Other")],
-                    kid = "Azimuth.labels", # subpopulation assignments
-                    gid = "condition",  # group IDs 
-                    sid = "sample",   # sample IDs 
-                    drop = TRUE)
-pb <-  aggregateData(prep.SCE, assay="logcounts",by = c("cluster_id", "sample_id"))
-
-mds_plot <- pbMDS(pb) + scale_color_manual(values=neuronal.color)
-ggsave(mds_plot, file=paste("snRNA_MDSplot_NegControls_neuronal_subclass.pdf",sep = ""), width = 20, height = 20, units = "cm")
+patched <- (mds_other | mds_negctrl) +  plot_layout(guides = "collect")
+ggsave("Peixoto_Figure4_Supplement4.jpg",  width = 20, height = 10, units = "cm")
+ggsave("Peixoto_Figure4_Supplement4.pdf",  width = 20, height = 10, units = "cm")
