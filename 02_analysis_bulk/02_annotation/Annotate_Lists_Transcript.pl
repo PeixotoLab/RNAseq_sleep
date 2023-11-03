d09e22a3a2d42f4ecfd65088e4e808ee623585ab
kaitlynford <74943440+kaitlynford@users.noreply.github.com>
2023-11-03 11:40:19 -0700

    Update Annotate_Lists_Transcript.pl

--- 02_analysis_bulk/02_annotation/Annotate_Lists_Transcript.pl
+++ 02_analysis_bulk/02_annotation/Annotate_Lists_Transcript.pl
@@ -1,8 +1,8 @@
 use strict;
 
 my $Annotations= "BioMart_Ensembl100_Gencodev25_GRCm38p6_with_Transcript_Name.txt";
-my $GeneList= "102522_Differential_Transcript_Expression_Significant_k=4.txt";
-my $outfile="022723_Annotated_Differential_Transcript_Expression_Significant_k=4.txt";
+my $GeneList= "DTE_Significant_k=4.txt";
+my $outfile="DTE_Significant_k=4_Annotated.txt";
 
 
 open IN,"<$Annotations" or die $!;
